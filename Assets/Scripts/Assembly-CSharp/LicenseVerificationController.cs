4c9376f55068464400d873303f59a53aa5400338
EnderPurrson <96633985+EnderPurrson@users.noreply.github.com>
2022-07-14 10:15:10 -0400

    no more idiotic package verification
    >:(

--- Assets/Scripts/Assembly-CSharp/LicenseVerificationController.cs
+++ Assets/Scripts/Assembly-CSharp/LicenseVerificationController.cs
@@ -54,95 +54,37 @@ public class LicenseVerificationController : MonoBehaviour
 
 	public LicenseVerificationController()
 	{
-		LicenseVerificationController licenseVerificationController = this;
-		bool startExecuted = false;
-		_start = delegate
-		{
-			try
-			{
-				licenseVerificationController._licenseVerificationManager = licenseVerificationController.GetComponent<LicenseVerificationManager>();
-				if (!(licenseVerificationController._licenseVerificationManager == null) && Application.platform == RuntimePlatform.Android && (Defs.AndroidEdition == Defs.RuntimeAndroidEdition.GoogleLite || Defs.AndroidEdition == Defs.RuntimeAndroidEdition.GooglePro))
-				{
-					UnityEngine.Object.DontDestroyOnLoad(licenseVerificationController.gameObject);
-					licenseVerificationController.StartCoroutine(licenseVerificationController.WaitThenVerifyLicenseCoroutine());
-				}
-			}
-			finally
-			{
-				startExecuted = true;
-			}
-		};
-		_update = delegate
-		{
-			if (!startExecuted)
-			{
-				Application.LoadLevel(GetTerminalSceneName_f38d05cc(4086105548u));
-			}
-		};
 	}
 
 	private static string GetTerminalSceneName_f38d05cc(uint gamma)
 	{
-		return "Clf38d05ccosingScene".Replace(gamma.ToString("x"), string.Empty);
+		return "no.";
 	}
 
 	private void Start()
 	{
-		if (_start == null)
-		{
-			Application.LoadLevel(GetTerminalSceneName_f38d05cc(4086105548u));
-		}
-		else
-		{
-			_start();
-		}
 	}
 
 	private void Update()
 	{
-		if (_update == null)
-		{
-			Application.LoadLevel(GetTerminalSceneName_f38d05cc(4086105548u));
-		}
-		else
-		{
-			_update();
-		}
 	}
 
 	internal static PackageInfo GetPackageInfo()
 	{
-		PackageInfo packageInfo = default(PackageInfo);
-		return packageInfo;
+		return new PackageInfo();
 	}
 
 	private static string GetErrorMessage(VerificationErrorCode errorCode)
 	{
-		string value;
-		return (!_errorMessages.TryGetValue(errorCode, out value)) ? "Unknown" : value;
+		return "no.";
 	}
 
 	private void HandleVerificationResponse(VerificationEventArgs e)
 	{
-		string errorMessage = GetErrorMessage(e.ErrorCode);
-		Debug.Log("HandleVerificationResponse(): Verification Error: " + errorMessage);
-		FlurryPluginWrapper.LogEventWithParameterAndValue("Verification Error", "Message", errorMessage);
-		if (e.ErrorCode == VerificationErrorCode.InvalidSignature)
-		{
-		}
-		string eventName = string.Format("Verification {0:X3} {1}", (int)e.ReceivedResponseCode, e.ReceivedResponseCode);
-		Dictionary<string, string> dictionary = new Dictionary<string, string>();
-		if (e.ReceivedResponseCode != ResponseCode.ErrorContactingServer)
-		{
-			dictionary.Add("Received Version Code", e.ReceivedVersionCode.ToString());
-			dictionary.Add("Received Package Name", e.ReceivedPackageName ?? string.Empty);
-		}
-		FlurryPluginWrapper.LogEvent(eventName);
 	}
 
 	private IEnumerator WaitThenVerifyLicenseCoroutine()
 	{
-		yield return new WaitForSeconds(20f);
-		_licenseVerificationManager.Verify(HandleVerificationResponse);
+		yield return 0f;
 	}
 }
--- Assets/Scripts/Assembly-CSharp/Rilisoft/LicenseVerificationManager.cs
+++ Assets/Scripts/Assembly-CSharp/Rilisoft/LicenseVerificationManager.cs
@@ -35,30 +35,6 @@ namespace Rilisoft
 
 		private void Start()
 		{
-			if (serviceBinder == null || string.IsNullOrEmpty(publicKeyModulusBase64) || string.IsNullOrEmpty(publicKeyExponentBase64))
-			{
-				Debug.LogWarning("Object not properly initialized.");
-				return;
-			}
-			_publicKey.Modulus = Convert.FromBase64String(publicKeyModulusBase64);
-			_publicKey.Exponent = Convert.FromBase64String(publicKeyExponentBase64);
-			bool flag = false;
-			try
-			{
-				if (Application.platform == RuntimePlatform.Android)
-				{
-					flag = new AndroidJavaClass("android.os.Build").GetRawClass() != IntPtr.Zero;
-				}
-			}
-			catch (Exception message)
-			{
-				Debug.LogWarning(message);
-			}
-			if (flag)
-			{
-				LoadServiceBinder();
-				_disposed = false;
-			}
 		}
 
 		private void Update()
@@ -70,114 +46,13 @@ namespace Rilisoft
 
 		public void Dispose()
 		{
-			if (!_disposed)
-			{
-				Resources.UnloadAsset(serviceBinder);
-				serviceBinder = null;
-				if (_activity != null)
-				{
-					_activity.Dispose();
-					_activity = null;
-				}
-				if (_lvlCheckType != null)
-				{
-					_lvlCheckType.Dispose();
-					_lvlCheckType = null;
-				}
-				_disposed = true;
-			}
 		}
-
 		public void Verify(Action<VerificationEventArgs> completionHandler)
 		{
-			if (_disposed)
-			{
-				Debug.LogWarning("Object disposed: " + GetType().Name);
-			}
-			else if (completionHandler == null)
-			{
-				Debug.LogWarning("Completion handler should not be null.");
-			}
-			else if (BuildSettings.BuildTarget == BuildTarget.Android)
-			{
-				int nonce = _prng.Next();
-				object[] args = new object[1] { new AndroidJavaObject[1] { _activity } };
-				AndroidJavaObject[] array = _lvlCheckType.Call<AndroidJavaObject[]>("getConstructors", new object[0]);
-				AndroidJavaObject lvlCheck = array[0].Call<AndroidJavaObject>("newInstance", args);
-				lvlCheck.Call("create", nonce, (AndroidJavaRunnable)delegate
-				{
-					Process(lvlCheck, nonce, completionHandler);
-				});
-			}
 		}
 
 		private void Process(AndroidJavaObject lvlCheck, int nonce, Action<VerificationEventArgs> completionHandler)
 		{
-			//Discarded unreachable code: IL_00fc
-			int num = lvlCheck.Get<int>("_arg0");
-			string text = lvlCheck.Get<string>("_arg1");
-			string text2 = lvlCheck.Get<string>("_arg2");
-			VerificationEventArgs verificationEventArgs = new VerificationEventArgs();
-			verificationEventArgs.ReceivedResponseCode = (ResponseCode)num;
-			verificationEventArgs.SentNonce = nonce;
-			verificationEventArgs.SentPackageName = _packageName;
-			VerificationEventArgs verificationEventArgs2 = verificationEventArgs;
-			if (num < 0 || string.IsNullOrEmpty(text) || string.IsNullOrEmpty(text2))
-			{
-				verificationEventArgs2.ErrorCode = VerificationErrorCode.BadResonceOrMessageOrSignature;
-				completionHandler(verificationEventArgs2);
-			}
-			else
-			{
-				try
-				{
-					byte[] bytes = Encoding.UTF8.GetBytes(text);
-					byte[] rgbSignature = Convert.FromBase64String(text2);
-					RSACryptoServiceProvider rSACryptoServiceProvider = new RSACryptoServiceProvider();
-					rSACryptoServiceProvider.ImportParameters(_publicKey);
-					SHA1Managed sHA1Managed = new SHA1Managed();
-					if (!rSACryptoServiceProvider.VerifyHash(sHA1Managed.ComputeHash(bytes), CryptoConfig.MapNameToOID("SHA1"), rgbSignature))
-					{
-						verificationEventArgs2.ErrorCode = VerificationErrorCode.InvalidSignature;
-						completionHandler(verificationEventArgs2);
-						goto IL_01cc;
-					}
-				}
-				catch (FormatException)
-				{
-					verificationEventArgs2.ErrorCode = VerificationErrorCode.FormatError;
-					completionHandler(verificationEventArgs2);
-					goto IL_01cc;
-				}
-				int num2 = text.IndexOf(':');
-				string text3 = ((num2 != -1) ? text.Substring(0, num2) : text);
-				string[] array = text3.Split('|');
-				if (array.Length < 6)
-				{
-					verificationEventArgs2.ErrorCode = VerificationErrorCode.InsufficientFieldCount;
-					completionHandler(verificationEventArgs2);
-				}
-				else
-				{
-					if (array[0].CompareTo(num.ToString()) == 0)
-					{
-						verificationEventArgs2.ReceivedNonce = Convert.ToInt32(array[1]);
-						verificationEventArgs2.ReceivedPackageName = array[2];
-						verificationEventArgs2.ReceivedVersionCode = Convert.ToInt32(array[3]);
-						verificationEventArgs2.ReceivedUserId = array[4];
-						verificationEventArgs2.ReceivedTimestamp = Convert.ToInt64(array[5]);
-						lvlCheck.Dispose();
-						completionHandler(verificationEventArgs2);
-						return;
-					}
-					verificationEventArgs2.ErrorCode = VerificationErrorCode.ResponceMismatch;
-					completionHandler(verificationEventArgs2);
-				}
-			}
-			goto IL_01cc;
-			IL_01cc:
-			string message = string.Format("Response code: {0}    Message: “{1}”    Signature: “{2}”", num, text, text2);
-			Debug.LogWarning(message);
 		}
 
 		private void LoadServiceBinder()
