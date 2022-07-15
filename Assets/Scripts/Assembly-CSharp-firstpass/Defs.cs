c10196beb414ea538c2bbaba136f02274375a2f7
EnderPurrson <96633985+EnderPurrson@users.noreply.github.com>
2022-07-15 12:26:55 -0400

    inside the lean is 2100000000 bucks
    REAL?!

--- Assets/Scripts/Assembly-CSharp-firstpass/Defs.cs
+++ Assets/Scripts/Assembly-CSharp-firstpass/Defs.cs
@@ -2644,6 +2644,7 @@ public static string CAnim(GameObject animator, string con){
 		levelNumsForMusicInMult.Add("Mine", 1048);
 		levelNumsForMusicInMult.Add("DevScene", 1049);
 		_premiumMaps.Add("Ants", 15);
+		_premiumMaps.Add("Lean_Matrix", 2100000000);
 		_premiumMaps.Add("Matrix", 15);
 		_premiumMaps.Add("Underwater", 15);
 		filterMaps.Add("Knife", 1);
