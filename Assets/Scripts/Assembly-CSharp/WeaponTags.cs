cf91d806a007cbcbd189f1a64df7ea15cec56257
NitrogenDioxide <kiffolisk@gmail.com>
2022-07-30 13:35:10 +0200

    sex

--- Assets/Scripts/Assembly-CSharp/WeaponTags.cs
+++ Assets/Scripts/Assembly-CSharp/WeaponTags.cs
@@ -432,6 +432,8 @@ public static class WeaponTags
 
 	public static readonly string AdvancedScoutRifle_Tag = "AdvancedScoutRifle";
 
+	public static readonly string Nokia_Tag = "Nokia";
+
 	public static readonly string OGUzi_Tag = "OGUzi";
 
 	public static readonly string GiantSword_Tag = "GiantSword";
