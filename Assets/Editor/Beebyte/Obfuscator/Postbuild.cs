d4849b955bd7a78e91960716b4702cfa304e3f0e
NitrogenDioxide <kiffolisk@gmail.com>
2022-08-26 21:17:49 +0200

    obfuscat

---
+++ Assets/Editor/Beebyte/Obfuscator/Postbuild.cs
@@ -0,0 +1,14 @@
+// This asset was uploaded by https://unityassetcollection.com
+
+
+namespace Beebyte.Obfuscator
+{
+	/**
+	 * Empty class to ensure old versions of this class are wiped out on upgrades.
+	 *
+	 * @See PipelineHook.cs for the build pipeline entry points
+	 */
+	public class Postbuild
+	{
+	}
+}
---
+++ Assets/OldOfficeDeveloperSkinController.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldOfficeDeveloperSkinController : MonoBehaviour
+{
+    public Material[] skins;
+}
