ecb9f596fefbbcd451e67d05c05408767beb2e58
EnderPurrson <96633985+EnderPurrson@users.noreply.github.com>
2022-08-30 00:29:33 -0400

    tesla pack from bg3d

--- Assets/Scripts/Assembly-CSharp/ItemDbRecords.cs
+++ Assets/Scripts/Assembly-CSharp/ItemDbRecords.cs
@@ -291,7 +291,8 @@ public static class ItemDbRecords
 		list.Add(new ItemRecord(403, "PiratPistols", "PiratPistols", "Weapon403", "PiratPistols", "PiratPistols", 300, true, false, "GemsCurrency"));
 		list.Add(new ItemRecord(406, "PirateMusket", "PirateMusket", "Weapon406", "PirateMusket", "PirateMusket", 350, true, false, "GemsCurrency"));
 		list.Add(new ItemRecord(284, "R01", "R01", "Weapon284", "R01", "R01", 350, true, false, "Coins"));
-		list.Add(new ItemRecord(284, "ioncannon", "ioncannon", "Weapon285", "ioncannon", "ioncannon", 800, true, false, "GemsCurrency"));
+		list.Add(new ItemRecord(285, "ioncannon", "ioncannon", "Weapon285", "ioncannon", "ioncannon", 800, true, false, "GemsCurrency"));
+		list.Add(new ItemRecord(287, "teslapack", "teslapack", "Weapon287", "teslapack", "teslapack", 500, true, false, "GemsCurrency"));
 		list.Add(new ItemRecord(283, "bg3dcodrailgun", "bg3dcodrailgun", "Weapon283", "bg3dcodrailgun", "bg3dcodrailgun", 700, true, false, "GemsCurrency"));
 		list.Add(new ItemRecord(436, "NightHunter", "NightHunter", "Weapon436", "NightHunter", "NightHunter", 700, true, false, "Coins"));
 		list.Add(new ItemRecord(437, "Casanova", "Casanova", "Weapon437", "Casanova", "Casanova", 400, true, false, "GemsCurrency"));
