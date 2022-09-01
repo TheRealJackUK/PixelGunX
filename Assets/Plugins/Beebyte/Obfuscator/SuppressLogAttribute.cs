31baea78fa5b4e4362561edb92732d43d82e3925
NitrogenDioxide <kiffolisk@gmail.com>
2022-09-01 21:02:07 +0200

    BRP

---
+++ Assets/Plugins/Beebyte/Obfuscator/SuppressLogAttribute.cs
@@ -0,0 +1,28 @@
+// This asset was uploaded by https://unityassetcollection.com
+
+
+
+using System;
+
+namespace Beebyte.Obfuscator
+{
+	/**
+	 * Suppresses certain messages (usually warnings) that the Obfuscator can output.
+	 */
+	[AttributeUsage(AttributeTargets.Method)]
+	public class SuppressLogAttribute : System.Attribute
+	{
+#pragma warning disable 414
+		private readonly MessageCode _messageCode;
+#pragma warning restore 414
+
+		private SuppressLogAttribute()
+		{
+		}
+
+		public SuppressLogAttribute(MessageCode messageCode)
+		{
+			_messageCode = messageCode;
+		}
+	}
+}
