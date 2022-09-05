925b82a4c5692b8e0838d2e8a52a5bf6649886e0
NitrogenDioxide <kiffolisk@gmail.com>
2022-09-05 12:25:03 +0200

    it works

--- Assets/Scripts/Assembly-CSharp/ObscuredPrefs.cs
+++ Assets/Scripts/Assembly-CSharp/ObscuredPrefs.cs
@@ -4,42 +4,20 @@ using System.Text;
 
 public class ObscuredPrefs : MonoBehaviour 
 {
-    public static string ConvertHex(String hexString)
+    public static string ConvertHex(string var1)
     {
-        try
-        {
-            string ascii = string.Empty;
-
-            for (int i = 0; i < hexString.Length; i += 2)
-            {
-                String hs = string.Empty;
-
-                hs   = hexString.Substring(i,2);
-                uint decval =   System.Convert.ToUInt32(hs, 16);
-                char character = System.Convert.ToChar(decval);
-                ascii += character;
-
-            }
-
-            return ascii;
-        }
-        catch (Exception ex) { Console.WriteLine(ex.Message); }
-
-        return string.Empty;
+        return var1;
     }
-    public static string Base64Encode(string plainText) {
-        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-        return System.Convert.ToBase64String(plainTextBytes);
+    public static string Base64Encode(string var1) {
+        var var2 = System.Text.Encoding.UTF8.GetBytes(var1);
+        return System.Convert.ToBase64String(var2);
     }
-    public static string Base64Decode(string base64EncodedData) {
-        var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
-        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+    public static string Base64Decode(string var1) {
+        var var2 = System.Convert.FromBase64String(var1);
+        return System.Text.Encoding.UTF8.GetString(var2);
     }
     public static string ToBytes(string var1) {
-        byte[] ba = Encoding.Default.GetBytes(var1);
-        var hexString = BitConverter.ToString(ba);
-        hexString = hexString.Replace("-", "");
-        return hexString;
+        return var1;
     }
     public static bool HasKey(string var1) {
         string var2 = Base64Encode(ToBytes(var1));
@@ -47,24 +25,31 @@ public class ObscuredPrefs : MonoBehaviour
     }
 
     public static void SetString(string var1, string var2) {
-        string var3 = Base64Encode(ToBytes(var1));
-        string var4 = Base64Encode(ToBytes(var2));
+        string var3 = Base64Encode(var1);
+        string var4 = Base64Encode(var2);
         PlayerPrefs.SetString(var3, var4);
     }
 
     // keep in mind this is setstring because you cant really store base64 in ints
     public static void SetInt(string var1, int var2) {
-        string var3 = Base64Encode(ToBytes(var1));
-        string var4 = Base64Encode(ToBytes(var2.ToString()));
+        string var3 = Base64Encode(var1);
+        string var4 = Base64Encode(var2.ToString());
         PlayerPrefs.SetString(var3, var4);
     }
     public static string GetString(string var1) {
-        string var2 = Base64Encode(ToBytes(var1));
-        return ConvertHex(Base64Decode(PlayerPrefs.GetString(var2)));
+        string var2 = Base64Encode(var1);
+        if (HasKey(var1)) {
+            return ConvertHex(Base64Decode(PlayerPrefs.GetString(var2)));
+        }
+        return "";
     }
 
     public static int GetInt(string var1) {
-        string var2 = Base64Encode(ToBytes(var1));
-        return int.Parse(ConvertHex(Base64Decode(PlayerPrefs.GetString(var2))));
+        string var2 = Base64Encode(var1);
+        if (HasKey(var1)) {
+            //UnityEngine.Debug.Log(ConvertHex(Base64Decode(PlayerPrefs.GetString(var2))));
+            return int.Parse(ConvertHex(Base64Decode(PlayerPrefs.GetString(var2))));
+        }
+        return 0;
     }
 }
